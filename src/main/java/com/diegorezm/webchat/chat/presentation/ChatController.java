63281851cc9ff55da5b0559760220561c5f2c6eb
diegorezm <dmourarezende@gmail.com>
2025-04-23 17:13:10 -0300

    showing index template

--- src/main/java/com/diegorezm/webchat/chat/presentation/ChatController.java
+++ src/main/java/com/diegorezm/webchat/chat/presentation/ChatController.java
@@ -1,7 +1,13 @@
 package com.diegorezm.webchat.chat.presentation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
 }
