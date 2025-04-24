9c4e97543d3aba1d894530d67e4f0065c11f01e9
diegorezm <dmourarezende@gmail.com>
2025-04-24 17:25:19 -0300

    no need to store the message

--- src/main/java/com/diegorezm/webchat/WebChatApplication.java
+++ src/main/java/com/diegorezm/webchat/WebChatApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebChatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebChatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebChatApplication.class, args);
+    }
 
 }
--- src/main/java/com/diegorezm/webchat/chat/presentation/ChatWebSocketHandler.java
+++ src/main/java/com/diegorezm/webchat/chat/presentation/ChatWebSocketHandler.java
@@ -1,6 +1,7 @@
 package com.diegorezm.webchat.chat.presentation;
 
 import com.diegorezm.webchat.chat.data.dto.ChatMessageDTO;
+import com.diegorezm.webchat.chat.domain.ChatMessage;
 import com.diegorezm.webchat.chat.domain.ChatMessageRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
@@ -33,7 +36,10 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
         String payload = message.getPayload();
         try {
             ChatMessageDTO chatMessageDTO = objectMapper.readValue(payload, ChatMessageDTO.class);
-            var newMessage = chatMessageRepository.save(chatMessageDTO);
+
+            DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+            String timestamp = LocalDateTime.now().format(formatter);
+            var newMessage = new ChatMessage(chatMessageDTO.author(), chatMessageDTO.content(), timestamp);
             for (WebSocketSession s : sessions) {
                 if (s.isOpen()) {
                     s.sendMessage(new TextMessage(
