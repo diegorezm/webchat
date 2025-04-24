8128e6080523fcf254d0efe1dd447219f83b5c5f
diegorezm <dmourarezende@gmail.com>
2025-04-23 21:45:22 -0300

    websocket config

---
+++ src/main/java/com/diegorezm/webchat/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.diegorezm.webchat.config;
+
+
+import com.diegorezm.webchat.chat.presentation.ChatWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/chat").setAllowedOrigins("http://localhost:8080");
+    }
+}
