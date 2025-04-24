200948f5228b3727e04e0f3f48bba6d28e26f17e
diegorezm <dmourarezende@gmail.com>
2025-04-23 21:45:20 -0300

    webscoket handler

---
+++ src/main/java/com/diegorezm/webchat/chat/presentation/ChatWebSocketHandler.java
@@ -0,0 +1,62 @@
+package com.diegorezm.webchat.chat.presentation;
+
+import com.diegorezm.webchat.chat.data.dto.ChatMessageDTO;
+import com.diegorezm.webchat.chat.domain.ChatMessageRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+    private final ChatMessageRepository chatMessageRepository;
+    private final ObjectMapper objectMapper;
+    private Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("WebSocket connection established: " + session.getId());
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        try {
+            ChatMessageDTO chatMessageDTO = objectMapper.readValue(payload, ChatMessageDTO.class);
+            var newMessage = chatMessageRepository.save(chatMessageDTO);
+            for (WebSocketSession s : sessions) {
+                if (s.isOpen()) {
+                    s.sendMessage(new TextMessage(
+                            objectMapper.writeValueAsString(
+                                    Map.of("message", newMessage)
+                            )
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        System.err.println("WebSocket transport error: " + exception.getMessage());
+        sessions.remove(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("Connection closed.");
+        sessions.remove(session);
+    }
+}
